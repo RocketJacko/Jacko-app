0aab8c519ffd65bfbebd2584e14c928a52925292
Jesus Carmona <jesus.carmona966@pascualbravo.edu.co>
2026-07-23 07:38:30 -0600

    style: remove numeric step counter from ActivateOverlay

--- src/components/canvas/ActivateOverlay.tsx
+++ src/components/canvas/ActivateOverlay.tsx
@@ -134,10 +134,6 @@ export function ActivateOverlay({ onStart }: { onStart?: () => void }) {
                   >
                     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"><path d="M15 18l-6-6 6-6"/></svg>
                   </button>
-                  
-                  <span className="activate-counter">
-                    {currentStep + 1} / {totalSteps}
-                  </span>
 
                   {!isLastStep ? (
                     <button
